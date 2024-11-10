0155da086f4cef985c96e3c2ec7c2db956e152ea
jzajas <jzajas1@gmail.com>
2024-11-10 18:17:50 +0100

    connecting to backend initial

--- Frames.py
+++ Frames.py
@@ -1,5 +1,8 @@
 from customtkinter import *
 from Services import *
+from tkinter import filedialog
+import pyperclip
+
 
 class UrlFrame(CTkFrame):
     def __init__(self, master, **kwargs):
@@ -8,10 +11,20 @@ class UrlFrame(CTkFrame):
         self.label = CTkLabel(master=self, text="URL", width=100, height=30, anchor="e",
                               text_color="#0080ff", fg_color="transparent", font=("Arial", 20), padx=30, pady=5)
         self.entry = CTkEntry(master=self, width=400, height=40, corner_radius=20, text_color="black",
-                              fg_color="white", placeholder_text="URL", placeholder_text_color="black", font=("Arial",15))
+                              fg_color="white", placeholder_text="URL", placeholder_text_color="black",
+                              font=("Arial", 15))
+        self.button = CTkButton(master=self, width=135, height=30, corner_radius=20, text_color="black",
+                                fg_color="white", font=("Arial", 15), text="Paste Url", command=self.paste_url)
 
-        self.label.grid(row=0,column=0)
-        self.entry.grid(row=1, column=0)
+        self.label.grid(row=0, column=0)
+        self.entry.grid(row=1, column=0, padx=3)
+        self.button.grid(row=1, column=1,  padx=3)
+
+    def paste_url(self):
+        url = pyperclip.paste()
+        if isinstance(url, str):
+            self.entry.delete(0, END)
+            self.entry.insert(0, url)
 
 
 class FileFrame(CTkFrame):
@@ -21,10 +34,22 @@ class FileFrame(CTkFrame):
         self.label = CTkLabel(master=self, text="File", width=100, height=30, anchor="e", text_color="#0080ff",
                               fg_color="transparent", font=("Arial", 20), padx=30, pady=5)
         self.entry = CTkEntry(master=self, width=400, height=40, corner_radius=20, text_color="black",
-                              fg_color="white", placeholder_text="File Path", placeholder_text_color="black", font=("Arial",15))
+                              fg_color="white", placeholder_text="File Path", placeholder_text_color="black",
+                              font=("Arial", 15))
+        self.button = CTkButton(master=self, width=135, height=30, corner_radius=20, text_color="black",
+                                fg_color="white", font=("Arial", 15), text="Browse Files", command=self.select_file)
 
         self.label.grid(row=0, column=0)
-        self.entry.grid(row=1, column=0)
+        self.entry.grid(row=1, column=0, padx=3)
+        self.button.grid(row=1, column=1,  padx=3)
+
+    def select_file(self):
+        self.entry.delete(0, END)
+        self.entry.insert(0, "Folder Path")
+        file = filedialog.askopenfile(title="Select File")
+        if file:
+            self.entry.delete(0, END)
+            self.entry.insert(0, file)
 
 
 class FolderFrame(CTkFrame):
@@ -34,10 +59,22 @@ class FolderFrame(CTkFrame):
         self.label = CTkLabel(master=self, text="Folder", width=100, height=30, anchor="e", text_color="#0080ff",
                               fg_color="transparent", font=("Arial", 20), padx=30, pady=5)
         self.entry = CTkEntry(master=self, width=400, height=40, corner_radius=20, text_color="black",
-                              fg_color="white", placeholder_text="Folder Path", placeholder_text_color="black", font=("Arial",15))
+                              fg_color="white", placeholder_text="Folder Path", placeholder_text_color="black",
+                              font=("Arial", 15))
+        self.button = CTkButton(master=self, width=100, height=30, corner_radius=20, text_color="black",
+                                fg_color="white", font=("Arial", 15), text="Browse Folders", command=self.select_folder)
 
         self.label.grid(row=0, column=0)
-        self.entry.grid(row=1, column=0)
+        self.entry.grid(row=1, column=0,  padx=3)
+        self.button.grid(row=1, column=1, padx=3)
+
+    def select_folder(self):
+        self.entry.delete(0, END)
+        self.entry.insert(0, "Folder Path")
+        folder = filedialog.askdirectory(title="Select Folder")
+        if folder:
+            self.entry.delete(0, END)
+            self.entry.insert(0, folder)
 
 
 # TODO to delete
@@ -48,7 +85,7 @@ class TestFrame(CTkFrame):
         self.label = CTkLabel(master=self, text="Folder", width=100, height=30, anchor="e",
                               fg_color="transparent", font=("Arial", 20), padx=30, pady=5)
         self.entry = CTkEntry(master=self, width=400, height=40, corner_radius=20,
-                              placeholder_text="Folder Path", font=("Arial",15))
+                              placeholder_text="Folder Path", font=("Arial", 15))
 
         self.label.grid(row=0, column=0)
         self.entry.grid(row=1, column=0)
@@ -57,9 +94,9 @@ class TestFrame(CTkFrame):
 class MiddleFrame(CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
-    #     TODO add progress bar?
+        # TODO add progress bar?
 
-        self.button = CTkButton(command=process_input)
-
-
+        self.button = CTkButton(master=self, width=100, height=30, corner_radius=20, text_color="black",
+                                font=("Arial", 15), text="Process input")
+        self.button.grid(row=3, column=0)
 
--- Window.py
+++ Window.py
@@ -2,19 +2,17 @@ from customtkinter import *
 from Frames import *
 from Services import *
 
-
 set_appearance_mode("system")
-set_default_color_theme("dark-blue")
+set_default_color_theme("green")
 
 
 class App(CTk):
     def __init__(self):
         super().__init__()
-        self.geometry("1300x500")
+        self.geometry("1400x500")
         # TODO Change name
         self.title("Face quality checking app ")
-
-        self.grid_rowconfigure(0, weight=0, minsize=150)
+        self.grid_rowconfigure(0, weight=0, minsize=135)
         self.grid_columnconfigure(0, weight=0, minsize=450)
 
         self.url_frame = UrlFrame(master=self)
@@ -31,10 +29,18 @@ class App(CTk):
         self.new_frame.grid(row=0, column=1)
 
         self.switch_var = StringVar(value="on")
-        self.mode_switch = CTkSwitch(self, text="Dark / Light Mode Switch", command=self.change_appearance, variable=self.switch_var,
-                                     onvalue="light", offvalue="dark")
+        self.mode_switch = CTkSwitch(self, text="Dark / Light Mode Switch", command=self.change_appearance,
+                                     variable=self.switch_var, onvalue="light", offvalue="dark")
         self.mode_switch.grid(row=3, column=0, padx=20, pady=30, sticky="nsew")
 
+        self.middle_frame = MiddleFrame(master=self)
+        self.middle_frame.grid(row=1, column=1, padx=20, pady=30, sticky="nsew")
+
+        self.process_button = CTkButton(master=self, width=100, height=30, corner_radius=20, text_color="black",
+                                        font=("Arial", 15), text="Process input", command=self.process_input)
+        self.process_button.grid(row=2, column=1, padx=20, pady=30, sticky="nsew")
+
+    # TODO add button that clears all fields and inserts base values there
     def change_appearance(self):
         mode = self.switch_var.get()
         if mode == "dark":
@@ -42,15 +48,19 @@ class App(CTk):
         elif mode == "light":
             set_appearance_mode("light")
 
-    def get_url(self):
-        return 0
+    def process_input(self):
+        url_path = self.url_frame.entry.get()
+        print(url_path)
 
-
+    # TODO might be useful
+    # if __name__ == '__main__':
+    #     root = tk.Tk()
+    #     app = Application(root)
+    #     app.mainloop()
 
 
 app = App()
 app.mainloop()
-
 
 # # TODO tabview dobre do pokazywania co jest dobre a co złe
 # TODO Frame dobre do oddzielenia sekcji gdzie są pokazywane wyniki
